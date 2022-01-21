043e0f46542a120a2d4ecdd7d4b7326dada46293
klevi <klevigjyrdyku@gmail.com>
2022-01-21 12:14:43 +0100

    komiti i 3 nga klevi

--- first.py
+++ first.py
@@ -1,8 +1,8 @@
-is_hot = False
-if is_hot:
-    print("It's a hot day")
-    print("Drink plenty of water")
+price = 100000
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = 0.1 * price
 else:
-    print("it's a cold day")
-    print("Wear warm clothes")
-print("Enjoy the day")
+    down_payment = 0.2 * price
+print(f"Down payment: ${down_payment}")
