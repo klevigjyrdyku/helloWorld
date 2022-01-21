f6c7dd53dc97e14b7750a33faffa2a04038ddf5f
klevi <klevigjyrdyku@gmail.com>
2022-01-21 12:17:42 +0100

    komiti i 2 nga klevi

--- first.py
+++ first.py
@@ -1,4 +1,8 @@
-x = 10**3
-print(x)
-
-
+is_hot = False
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water")
+else:
+    print("it's a cold day")
+    print("Wear warm clothes")
+print("Enjoy the day")
