9866ed6fd13ec320f704a9178d644e9f9d92d442
klevi <klevigjyrdyku@gmail.com>
2022-01-25 15:32:56 +0100

    komiti i 6.4 nga klevi(problem)

--- first.py
+++ first.py
@@ -1,25 +1,6 @@
-command = ""
-Started = False
-while True:
-    command = input("< "). lower()
-    if command == "start":
-        if Started:
-            print("Car is alredy started!")
-        else:
-            Started = True
-            print("car started...")
-    elif command == "stop":
-        if not Started:
-            print("Car is alredy stopped!")
-        else:
-            print("Car stopped...")
-    elif command == "help":
-        print("""
-        start - to start the car
-        stop -  to stop the car
-        quit - to quit
-        """)
-    elif command == "quit":
-              break
-    else:
-        print("Sorry i don't andestand that!??")
+prices = [10, 20, 30]
+
+total = 0
+for price in prices:
+    total += price
+print(f"total: {}")
