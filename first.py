2e7b8d05cc81cf7acd063d76e90cbdefd780ffc9
klevi <klevigjyrdyku@gmail.com>
2022-01-27 13:23:16 +0100

    komiti i 7.2 nga klevi

--- first.py
+++ first.py
@@ -1,5 +1,7 @@
-numbers = [2, 5, 6, 8, 1, 2, 2, 7]
-numbers2 = numbers.copy()
-numbers.append(10)
-print(numbers2)
-print(numbers)
+numbers = [2, 5, 6, 8, 1, 2, 7]
+uniques = []
+for number in numbers:
+    if number not in uniques:
+        uniques.append(number)
+print(uniques)
+
