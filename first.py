a374abb2b7b459579920b2cefe1a2ee862202acb
klevi <klevigjyrdyku@gmail.com>
2022-01-28 11:45:37 +0100

    komiti i 7.3 nga klevi

--- first.py
+++ first.py
@@ -1,13 +1,8 @@
-string = "klevi gjyrdyku";
-print(len(string))
-for i in range(0, len(string)):
-    count = 1;
-    for j in range(i + 1, len(string)):
-        if (string[i] == string[j] and string[i] != ' '):
-            count = count + 1;
-            string = string[:j] + '0' + string[j + 1:];
-
-    if (count > 1 and string[i] != '0'):
-        print(string[i], " - ", count);
-
+counts =  'klevi gjyrdyku'
+uniques = []
+for str in counts:
+    if str not in uniques:
+        uniques.append(str)
+print(uniques)
+print(len(counts))
 
