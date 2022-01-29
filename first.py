6616b0d9704cfbe9e4db23035aa9e5369146bf3b
klevi <klevigjyrdyku@gmail.com>
2022-01-29 17:58:14 +0100

    komiti i 7.6 nga klevi

--- first.py
+++ first.py
@@ -1,10 +1,7 @@
-message = input(">")
-words = message.split(' ')
-emojis = {
-    ":)": "8",
-    ":(": "7"
-}
-outpute = ""
-for word in words:
-    outpute  += emojis.get(word, word ) + " "
-print(outpute)
+def greet_users():
+    print('Hi there')
+    print('How are you')
+print("start")
+greet_users()
+print("Finish")
+
