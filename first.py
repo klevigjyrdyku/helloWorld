94aab0fa36d7bfd4d3a07de3c04feb674ce0eee0
klevi <klevigjyrdyku@gmail.com>
2022-02-17 13:31:02 +0100

    komiti i 8.1 nga klevi

--- first.py
+++ first.py
@@ -1,14 +1,28 @@
-def prime_checker(number):
-    is_prime = True
-    for i in range(2, number):
-        if number % 1 == 0:
-            is_prime = False
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+bill = 0
+S = 15
+M = 20
+L = 25
+if size == "S":
+  bill += S
+  if add_pepperoni == "Y":
+    bill += 2
+  if extra_cheese == "Y":
+      bill += 1
+elif size == "M":
+  bill += M
+  if add_pepperoni == "Y":
+    bill += 3
+  if extra_cheese == "Y":
+      bill += 1
+else :
+  bill += L
+  if add_pepperoni == "Y":
+    bill += 3
+  if extra_cheese == "Y":
+      bill += 1
 
-    if is_prime:
-        print("It's a prime number.")
-    else:
-        print("It's not a prime number.")
-
-
-n = int(input("Check this number: "))
-prime_checker(number=n)
+print(f"Your final bill is: ${bill}.")
